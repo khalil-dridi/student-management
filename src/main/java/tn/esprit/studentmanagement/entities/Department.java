6df697e64cbb35accdd57d28df899a108fd0bdc6
khalil-dridi <khalil.dridi@esprit.tn>
2025-12-12 02:24:36 +0100

    Initial commit

---
+++ src/main/java/tn/esprit/studentmanagement/entities/Department.java
@@ -0,0 +1,26 @@
+package tn.esprit.studentmanagement.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Department {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idDepartment;
+    private String name;
+    private String location;
+    private String phone;
+    private String head; // chef de département
+
+    @OneToMany(mappedBy = "department")mvn clean install
+
+    private List<Student> students;
+}
